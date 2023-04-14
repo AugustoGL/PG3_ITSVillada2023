5283b4f2ba54dfbfe2fe99a20230e32394f89da4
Augusto Guchea <aguchealezama@gmail.com>
2023-04-14 12:05:40 -0300

    Ejercicio_4-Guia_3

---
+++ Ejercicios/3_Manejo_de_exceptiones_con_python/Ejercicio_4-Guchea.py
@@ -0,0 +1,11 @@
+def division():
+    try:
+        num1 = float(input("Ingrese el primer numero: "))
+        num2 = float(input("Ingrese el segundo numero: "))
+        print("La division es: ", num1/num2)
+    except ValueError:
+        print("Solo se permiten numeros")
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0")
+
+division()
